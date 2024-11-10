84e8ee08bcb485af2f2ebb0cdefd6c17bfaef7c8
hongnganpt-0481 <pham.thi.hong.ngan@sun-asterisk.com>
2024-11-11 00:57:34 +0700

    fix

--- src/index.ts
+++ src/index.ts
@@ -89,9 +89,6 @@ app.use(function (
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
 });
 
 export default app;
